5e539b64b0102f3e26f82392c5b895742f13c34a
ashfaqshimer <shimer.ahamed@gmail.com>
2019-10-07 15:47:42 +0530

    Initialize project

---
+++ components/_App/Layout.js
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import { Container } from 'semantic-ui-react';
+
+import Header from './Header';
+import HeadContent from './HeadContent';
+
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Head>
+				<HeadContent />
+				{/* StyleSheets */}
+				<link
+					rel='stylesheet'
+					type='text/css'
+					href='/static/styles.css'
+				/>
+				<link
+					rel='stylesheet'
+					type='text/css'
+					href='/static/nprogress.css'
+				/>
+				<link
+					rel='stylesheet'
+					href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css'
+				/>
+				<title>Royal Merchandise | Simply the Best!</title>
+			</Head>
+			<Header />
+			<Container text>{children}</Container>
+		</>
+	);
+};
+
+export default Layout;
