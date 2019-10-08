37bfc1a5de8b05ce03c56648b5fecdb456554b92
ashfaqshimer <shimer.ahamed@gmail.com>
2019-10-08 13:03:31 +0530

    Add mongoDB connection. Setup environment variables

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -25,35 +25,46 @@ const Header = () => {
 						Royal Merchandise
 					</Menu.Item>
 				</Link>
-				<Link href='/cart'>
-					<Menu.Item header active={isActive('/cart')}>
-						<Icon name='cart' size='large' /> Cart
-					</Menu.Item>
-				</Link>
-				<Link href='/create'>
-					<Menu.Item header active={isActive('/create')}>
-						<Icon name='add square' size='large' /> Create
-					</Menu.Item>
-				</Link>
-				<Link href='/account'>
-					<Menu.Item header active={isActive('/account')}>
-						<Icon name='user' size='large' /> Account
-					</Menu.Item>
-				</Link>
 
-				<Menu.Item header>
-					<Icon name='sign out' size='large' /> Log Out
-				</Menu.Item>
-				<Link href='/signin'>
-					<Menu.Item header active={isActive('/signin')}>
-						<Icon name='sign in' size='large' /> Log In
-					</Menu.Item>
-				</Link>
-				<Link href='/signup'>
-					<Menu.Item header active={isActive('/signup')}>
-						<Icon name='signup' size='large' /> Signup
-					</Menu.Item>
-				</Link>
+				<Menu.Menu position='right'>
+					<Link href='/cart'>
+						<Menu.Item header active={isActive('/cart')}>
+							<Icon name='cart' size='large' /> Cart
+						</Menu.Item>
+					</Link>
+					<Link href='/create'>
+						<Menu.Item header active={isActive('/create')}>
+							<Icon name='add square' size='large' /> Create
+						</Menu.Item>
+					</Link>
+
+					{user ? (
+						<>
+							<Link href='/account'>
+								<Menu.Item header active={isActive('/account')}>
+									<Icon name='user' size='large' /> Account
+								</Menu.Item>
+							</Link>
+
+							<Menu.Item header>
+								<Icon name='sign out' size='large' /> Log Out
+							</Menu.Item>
+						</>
+					) : (
+						<>
+							<Link href='/signin'>
+								<Menu.Item header active={isActive('/signin')}>
+									<Icon name='sign in' size='large' /> Log In
+								</Menu.Item>
+							</Link>
+							<Link href='/signup'>
+								<Menu.Item header active={isActive('/signup')}>
+									<Icon name='signup' size='large' /> Signup
+								</Menu.Item>
+							</Link>
+						</>
+					)}
+				</Menu.Menu>
 			</Container>
 		</Menu>
 	);
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-const index = () => {
+const Index = ({ products }) => {
+	console.log(products);
+
 	return <div>Home</div>;
 };
 
-export default index;
+Index.getInitialProps = async () => {
+	// fetch data on server
+	const url = 'http://localhost:3000/api/products';
+	const response = await axios.get(url);
+	// return data as an object
+	return { products: response.data };
+	// this object will be merged with existing props
+};
+
+export default Index;
