7b7203402809d78bcdf7eb399151b6b66e5b5d79
Shimer <ashfaq.shimer@itelasoft.com>
2019-11-07 17:46:41 +0530

    Create account header, order history

--- pages/account.js
+++ pages/account.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import AccountHeader from '../components/Account/AccountHeader';
+import AccountOrders from '../components/Account/AccountOrders';
+import baseUrl from '../utils/baseUrl';
+import { parseCookies } from 'nookies';
+import axios from 'axios';
 
-const account = () => {
-	return <div>Account</div>;
+const Account = ({ user, orders }) => {
+  console.log(orders);
+  return (
+    <div>
+      <AccountHeader {...user} />
+      <AccountOrders orders={orders} />
+    </div>
+  );
 };
 
-export default account;
+Account.getInitialProps = async (ctx) => {
+  const { token } = parseCookies(ctx);
+  if (!token) {
+    return { orders: [] };
+  }
+  const payload = { headers: { Authorization: token } };
+  const url = `${baseUrl}/api/orders`;
+  const response = await axios.get(url, payload);
+  return response.data;
+};
+
+export default Account;
