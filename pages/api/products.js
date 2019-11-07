597d2eeb34066f7d9781aabb19fecd9182fa8cb4
Shimer <ashfaq.shimer@itelasoft.com>
2019-11-07 16:29:31 +0530

    Add pagination for product page

--- pages/api/products.js
+++ pages/api/products.js
@@ -5,27 +5,39 @@ import Product from '../../models/Product';
 connectDb();
 
 export default async (req, res) => {
-	try {
-		const products = await Product.find();
+  try {
+    const products = await Product.find();
 
-		// If product list is empty manually export them
-		if (!products.length) {
-			console.log('Executing this statement');
+    // If product list is empty manually export them
+    if (!products.length) {
+      PRODUCTS.forEach(async (product) => {
+        const prod = new Product();
+        prod.name = product.name;
+        prod.price = product.price;
+        prod.description = product.description;
+        prod.sku = product.sku;
+        prod.mediaUrl = product.mediaUrl;
 
-			PRODUCTS.forEach(async (product) => {
-				const prod = new Product();
-				prod.name = product.name;
-				prod.price = product.price;
-				prod.description = product.description;
-				prod.sku = product.sku;
-				prod.mediaUrl = product.mediaUrl;
+        return await prod.save();
+      });
+    }
+    const { page, size } = req.query;
+    const pageNum = Number(page);
+    const pageSize = Number(size);
+    let paginatedProducts = [];
+    const totalDocs = await Product.countDocuments();
+    const totalPages = Math.ceil(totalDocs / pageSize);
+    if (pageNum === 1) {
+      paginatedProducts = await Product.find().limit(pageSize);
+    } else {
+      const skips = pageSize * (pageNum - 1);
+      paginatedProducts = await Product.find()
+        .skip(skips)
+        .limit(pageSize);
+    }
 
-				return await prod.save();
-			});
-		}
-
-		res.status(200).json(products);
-	} catch (err) {
-		console.log(err);
-	}
+    res.status(200).json({ products: paginatedProducts, totalPages });
+  } catch (err) {
+    console.log(err);
+  }
 };
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
+import ProductPagination from '../components/Index/ProductPagination';
 import baseUrl from '../utils/baseUrl';
 
-const Index = ({ products }) => {
-	return (
-		<div>
-			<ProductList products={products} />
-		</div>
-	);
+const Index = ({ products, totalPages }) => {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 };
 
-Index.getInitialProps = async () => {
-	// fetch data on server
-	const url = `${baseUrl}/api/products`;
-	const response = await axios.get(url);
-	// return data as an object
-	return { products: response.data };
-	// this object will be merged with existing props
+Index.getInitialProps = async (ctx) => {
+  console.log(ctx.query);
+  const page = ctx.query.page ? ctx.query.page : '1';
+  const size = 9;
+  // fetch data on server
+  const url = `${baseUrl}/api/products`;
+  const payload = { params: { page, size } };
+  const response = await axios.get(url, payload);
+  // return data as an object
+  return response.data;
+  // this object will be merged with existing props
 };
 
 export default Index;
