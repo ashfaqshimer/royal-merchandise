58c4c5583850e43f0f98b3d5e2479cd5e7aee07f
Shimer <ashfaq.shimer@itelasoft.com>
2019-11-08 13:44:04 +0530

    Fix typo in baseUrl for production alias

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://royalmerchandise.now.sh.now.sh'
+    ? 'https://royalmerchandise.now.sh'
     : 'http://localhost:3000';
 
 export default baseUrl;
